f3961f8caf27c5e12f50ae78959ea634d7cc3997
Franco Rivera <francoriverarivas@hotmail.com>
2020-06-20 16:11:16 -0500

    added tailwind styles

--- repository/house_repository.go
+++ repository/house_repository.go
@@ -2,7 +2,8 @@ package repository
 
 import (
 	"errors"
-	"../models")
+	"../models"
+	"strings")
 
 type HouseRepository struct{
 	//Repository
@@ -12,6 +13,9 @@ type HouseRepository struct{
 var houseList = []models.House{
 	models.House{ID: 1, Title: "House 1", Content: "House 1 Listing "},
 	models.House{ID: 2, Title: "House 2", Content: "House 2 Listing "},
+	models.House{ID: 3, Title: "House 3", Content: "House 3 Listing "},
+	models.House{ID: 4, Title: "House 4", Content: "House 4 Listing "},
+	models.House{ID: 5, Title: "House 5", Content: "House 5 Listing "},
 }
 
 
@@ -29,3 +33,18 @@ func (r *HouseRepository) FetchById(id int) (*models.House, error) {
 	}
 	return nil, errors.New("House not found")
 }
+
+
+// not implemented
+
+func (r *HouseRepository) FindByTitle(title string) ([]models.House, error) {
+	matchedList := []models.House;
+	//r.Repository.FetchAll();
+	for _, o := range houseList{
+		if strings.Contains(o.Title, title){
+			matchedList = append(matchedList, o)
+		}
+	}
+	return matchedList
+}
+
